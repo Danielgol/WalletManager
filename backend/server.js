b3d3adbf425f43cf1be8eeb558eddf1632c0d11e
Daniel Rosa <dan.rosa10@gmail.com>
2021-08-04 22:14:53 -0300

    Fazendo comunicação com API btc

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,29 @@
-const express = require('express');
 
+const express = require('express');
 const app = express();
+const https = require('https');
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/',(req,res) =>{
-	res.send('We are on home!');
+app.get('/', (req, res) =>{
+
+	const api_url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+
+	https.get(api_url, (resp) => {
+		let data = '';
+
+		resp.on('data', (chunk) => {
+			data += chunk;
+		});
+
+		resp.on('end', () => {
+			data = JSON.parse(data);
+			var btc_value = parseFloat(data.bpi.USD.rate.replace(',',''));
+			console.log(btc_value);
+			res.send(btc_value+'');
+		});
+	});
+
 });
 
 app.listen(PORT, () => console.log('Server started on port '+PORT));
