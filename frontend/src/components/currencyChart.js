efa85fb7b39e9b234942fe320aceac2b76b4d5f6
Daniel Rosa <dan.rosa10@gmail.com>
2021-08-18 11:10:53 -0300

    tentando com react native svg

--- frontend/src/components/currencyChart.js
+++ frontend/src/components/currencyChart.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity,
-        Button, FlatList, TextInput, Dimensions } from 'react-native'
+        Button, FlatList, TextInput, Dimensions } from 'react-native';
+import { LineChart, Grid } from 'react-native-svg-charts';
+import * as shape from 'd3-shape';
 
 
 const { width, height } = Dimensions.get("screen");
@@ -17,23 +19,34 @@ function handleEditChange(amount, props){
 }
 
 
-const CurrencyChart = (props) => {
+class CurrencyChart extends React.PureComponent{
+    render(){
+        const data = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80];
 
-    return(
-        <View style={styles.screen}>
-        </View>
-    );
+        return(
+            <View style={styles.screen}>
+                <LineChart
+                    style={{ top: 10, height: '80%', width: '90%'}}
+                    data={data}
+                    contentInset={{ top: 30, bottom: 30 }}
+                    svg={{ stroke: '#40970A', strokeWidth: 2.4}}
+                    curve={shape.curveMonotoneX}
+                    >
+                </LineChart>
+            </View>
+        );
+    }
 }
+
 
 const styles = StyleSheet.create({
     screen: {
-        width: width*0.98,
         alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#303030',
         height: height*0.3,
+        width: width*0.98,
         borderRadius: 20,
         elevation: 10,
+        backgroundColor: '#ddd'
     },
     row: {
         width: '100%',
