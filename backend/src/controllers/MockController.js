627f9895d90a7f7cb1e2531ac5bdb573b8030b90
Daniel Rosa <dan.rosa10@gmail.com>
2021-08-21 00:00:44 -0300

    foo

--- backend/src/controllers/MockController.js
+++ backend/src/controllers/MockController.js
@@ -3,12 +3,12 @@ const https = require('https');
 
 const banco = [
     {name: 'B-BRA', value: 1285.00, prefix: 'BRL', key: '1'},
-    {name: 'CCTRF', value: 840.00, prefix: 'BRL', key: '2'},
+    {name: 'CCTRF', value: 640.00, prefix: 'BRL', key: '2'},
     {name: 'CAIXA', value: 4760.00, prefix: 'BRL', key: '3'},
     {name: 'CASA', value: 300.00,  prefix: 'USD', key: '4'},
-    {name: 'BTC', value: 0.005020, prefix: 'BTC', key: '5'},
-    {name: 'BTC-RIPIO', value: 0.00040, prefix: 'BTC', key: '6'},
-    {name: 'ETH-RIPIO', value: 0.00312, prefix: 'ETH', key: '7'}
+    {name: 'BTC', value: 0.00502102, prefix: 'BTC', key: '5'},
+    {name: 'BTC-RIPIO', value: 0.00115792, prefix: 'BTC', key: '6'},
+    {name: 'ETH-RIPIO', value: 0.00312455, prefix: 'ETH', key: '7'}
 ];
 
 
